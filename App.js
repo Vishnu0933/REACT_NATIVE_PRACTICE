0e5d52e733aa0eb9505ea44dbf8d41ee9eda4e42
Vishnu0933 <sontakkevg@gmail.com>
2024-04-08 11:30:59 +0530

    index and app commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View} from 'react-native';
+import Hooks from './component/Hooks';
+
+const App = () => {
+  return (
+    <View style={{flex: 1}}>
+      <Hooks />
+    </View>
+
+  );
+};
+
+export default App;
